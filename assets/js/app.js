6428fdb27cdcf470e67af580b8f21adcf4477d67
AqibZahid01 <aqibkhanpuriya@gmail.com>
2023-09-28 14:30:07 +0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+function first() {
+    document.getElementById("changeimg").src = "../assets/Images/foot.jpeg";
+    document.getElementById("changehead").innerHTML = "First Training Class";
+    document.getElementById("changepara").innerHTML = "Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.";
+}
+function Second() {
+    document.getElementById("changeimg").src = "../assets/Images/man-1.jpg";
+    document.getElementById("changehead").innerHTML = "Second Training Class";
+    document.getElementById("changepara").innerHTML = "Integer dapibus, est vel dapibus mattis, sem mauris luctus leo, ac pulvinar quam tortor a velit. Praesent ultrices erat ante, in ultricies augue ultricies faucibus. Nam tellus nibh, ullamcorper at mattis non, rhoncus sed massa. Cras quis pulvinar eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.";
+}
+function Third() {
+    document.getElementById("changeimg").src = "../assets/Images/man-3.jpg";
+    document.getElementById("changehead").innerHTML = "Third Training Class";
+    document.getElementById("changepara").innerHTML = "Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.";
+}
+function Fourth() {
+    document.getElementById("changeimg").src = "../assets/Images/man-4.jpeg";
+    document.getElementById("changehead").innerHTML = "Fourth Training Class";
+    document.getElementById("changepara").innerHTML = "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultrices elementum odio ac tempus. Etiam eleifend orci lectus, eget venenatis ipsum commodo et.";
+}
+function Monday() {
+    document.getElementById('monday1').innerHTML = '10:00AM - 11:30AM';
+    document.getElementById('monday2').innerHTML = '2:00PM - 3:30PM';
+}
+function Tuesday() {
+    document.getElementById('tuesday1').innerHTML = '2:00PM - 3:30PM';
+    document.getElementById('tuesday2').innerHTML = '10:00AM - 11:30AM';
+}
+function Monday() {
+    document.getElementById('monday1').innerHTML = '10:00AM - 11:30AM';
+    document.getElementById('monday2').innerHTML = '2:00PM - 3:30PM';
+}
+function Monday() {
+    document.getElementById('monday1').innerHTML = '10:00AM - 11:30AM';
+    document.getElementById('monday2').innerHTML = '2:00PM - 3:30PM';
+}
+function Monday() {
+    document.getElementById('monday1').innerHTML = '10:00AM - 11:30AM';
+    document.getElementById('monday2').innerHTML = '2:00PM - 3:30PM';
+}
